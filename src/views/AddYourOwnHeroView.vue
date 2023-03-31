dff3f260ed4dce35c90c22ca7d25c5e1f5176a8a
Israel Soares <israeldiassoares@gmail.com>
2023-03-31 12:56:05 +0200

    setup project Vite|TailWind|Tests|Routes

---
+++ src/views/AddYourOwnHeroView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1 class="text-3xl font-bold underline">
+      Add your own hero
+    </h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
